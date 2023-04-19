aa85e8aa11c95e573bfd67cadd186fe6fc4222ef
Christian Aquino <christianaquinomoreta2@gmail.com>
2023-04-19 19:15:11 -0400

    Comentario

--- IDS328L/Services/PersonaServices.cs
+++ IDS328L/Services/PersonaServices.cs
@@ -23,7 +23,7 @@ namespace IDS328L.Services
         {
             Context = _Context;
         }
-        // Esto es un comentario mejorado 2.0
+        // Esto es un comentario mejorado 2.1
 
         #region This method return one record or List of Persons
         public Response<ViewPersona> Get(int Operacion, int Id, bool Estado)
